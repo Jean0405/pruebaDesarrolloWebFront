55ad80b7c36d4cf4b18bd1b87189a4d9a5645599
Jean0405 <jeanpierrea0405@gmail.com>
2023-12-20 09:23:36 -0500

    feat: :sparkles: se añadieron las rutas para cada vista

--- src/router.js
+++ src/router.js
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from './components/pages/Login.vue';
+import Login from "./components/pages/Login.vue";
+import Register from "./components/pages/Register.vue";
+import ProfessionalPage from "./components/pages/ProfessionalPage.vue";
+import PatientPage from "./components/pages/PatientPage.vue";
+import UpdatePassword from "./components/pages/UpdatePassword.vue";
+import UpdateProfile from "./components/pages/UpdateProfile.vue";
 
 const routes = [
-    { path: '/login', component: Login}
+  { path: "/", component: Login },
+  { path: "/register", component: Register },
+  { path: "/professionalPage", component: ProfessionalPage },
+  { path: "/patientPage", component: PatientPage },
+  { path: "/updatePassword", component: UpdatePassword },
+  { path: "/updateProfile", component: UpdateProfile },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
