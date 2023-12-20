9fd7151d2d5cb64a285c34005cb25a2243a8669b
Jean0405 <jeanpierrea0405@gmail.com>
2023-12-20 09:22:11 -0500

    feat: :heavy_plus_sign: se agrego fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 app.use(router);
-app.mount('#app');
+app.mount("#app");
